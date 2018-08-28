fbc9ecffd115540be3362a765e69692fa202cec4
RichFisher <rich.fishernc@gmail.com>
2018-08-27 21:17:37 -0500

    added objectchallenge

---
+++ objectchallenge.js
@@ -0,0 +1,44 @@
+let elizabethSanger = {
+    congressionalDistrict: 5,
+    platform: {
+        taxes:'Taxes, who needs em?',
+        jobs: 'People who like jobs like Sanger',
+        infrastructure: 'Infrastructure, we mentioned it!',
+        health: 'Remember, if you support health care, you support communism',
+        crime: 'we got a lot of crime, but hey, its not my fault' },
+    donationURL: 'http://www.wasteyourcash.org',
+    calendar: [ 
+        {
+            date:
+            title:
+            description:
+        }
+    ],
+    volunteers: [ 
+        {
+            name: 'Joe',
+            address: '101 joe st.',
+            email: 'joe@joe.com',
+            phone: '555-5555',
+            availability: 'always',
+            activities: 'whatever is neccassary' },
+        { 
+            name: 'Jill'
+            address: "202 jill st."
+            email: 'jill@jill.com'
+            phone: '444-4444'
+            availability: 'never'
+            activities: 'absolutely nothing' } ],
+    biography: 'I love this state I moved to 3 months ago.',
+    images: [ {
+            image: 'www.headshots.com/headshot.jpg',
+            description: 'the headshot' },
+        {
+            image: 'www.familypic.com/family.jpg',
+            description: 'the family' },
+        {
+            image: 'www.constituents.com/constituent.jpg',
+            description: 'the constituents' } ],
+    mission: 'Make myself look good',
+    voteURL: 'http://www.votebutonlyforSanger.org'
+}
